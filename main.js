cf7a1785c2364aa1e9f8f13a2013b8d8c3607610
DannyCalde <ldca.sud@gmail.com>
2025-03-21 13:46:30 -0600

    main.js

--- main.js
+++ main.js
@@ -1,21 +1,4 @@
-window.onload = () => {
-    document.body.classList.remove("container");
-
-    const audio = document.getElementById("myAudio");
-    const playButton = document.getElementById("playButton");
-
-    audio.play().catch(error => console.log("Error al reproducir el audio:", error));
-
-    playButton.addEventListener("click", () => {
-      if (audio.paused) {
-        audio.currentTime = 0; // Reinicia la música al principio
-        audio.play().catch(error => console.log("Error al reproducir el audio:", error));
-        playButton.textContent = "Pausar música";
-      } else {
-        audio.pause();
-        playButton.textContent = "Reproducir música";
-      }
-   });
+onload = () =>{
+        document.body.classList.remove("container");
 };
-
 
